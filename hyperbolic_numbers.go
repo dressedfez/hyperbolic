6831022a42da82738f663d185d6a03b87203eb6b
Frank Zimmer <frank.e.zimmer@gmail.com>
2020-12-09 21:01:49 +0100

    Make it accessible from outside

--- hyperbolic_numbers.go
+++ hyperbolic_numbers.go
@@ -1,35 +1,31 @@
 package hyperbolic
 
-type Hypnum interface {
-	Add(summand Hypnum) Hypnum
-	NewHypNum(x, y float64) Hypnum
-}
-
-type hypnum struct {
+// Hypnum is the structure for hyperbolic numbers
+type Hypnum struct {
 	x, y float64
 }
 
-// method that defines the addition of
+// Add method defines the addition of
 // hyperbolic numbers
-func (h hypnum) Add(summand hypnum) hypnum {
-	var result hypnum
+func (h Hypnum) Add(summand Hypnum) Hypnum {
+	var result Hypnum
 	result.x = h.x + summand.x
 	result.y = h.y + summand.y
 	return result
 }
 
-// method that defines the multiplication of
+// Multiply method defines the multiplication of
 // hyperbolic numbers
-func (h hypnum) Multiply(factor hypnum) hypnum {
-	var result hypnum
+func (h Hypnum) Multiply(factor Hypnum) Hypnum {
+	var result Hypnum
 	result.x = h.x*factor.x + h.y*factor.y
 	result.y = h.x*factor.y + h.y*factor.x
 	return result
 }
 
-// creates an hyperbolic number entity
-func NewHypNum(x, y float64) hypnum {
-	return hypnum{
+// NewHypNum creates a hyperbolic number
+func NewHypNum(x, y float64) Hypnum {
+	return Hypnum{
 		x: x,
 		y: y,
 	}
